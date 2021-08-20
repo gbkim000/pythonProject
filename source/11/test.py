630975dfc8712999c1658a1e18f6e36dd2f8b314
GieBoong Kim <gbkim1924@gmail.com>
2021-08-20 09:17:58 +0900

    8월 20일 오후 source 폴더 커밋

---
+++ source/11/test.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+data=[20,30,40]
+x=[1,2,3]
+plt.plot(x,data)
+plt.show()
---
+++ source/12/ex12-1.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+data = [20, 30, 70]
+x = [1, 2, 3]
+plt.plot(x, data)
+plt.show()
