aac1b78b84379d4a9f258931e4765f3f44402cb4
gbkim000 <gbkim1924@gmail.com>
2021-08-21 23:59:11 +0900

    8월 21일 오후 KMP 알고리즘 수정

--- StrSearch/KMPmatch.py
+++ StrSearch/KMPmatch.py
@@ -1,47 +1,70 @@
 def kmp_match(txt: str, pat: str) -> int:
-    pt = 1
-    pp = 0
+    index = 1
+    cnt = 0
     skip = [0] * (len(pat) + 1)
 
-    skip[pt] = 0
-
+    skip[index] = 0
+    skip[0] = -1
     # ----- 교재에 있는 코드 -----
-    while pt != len(pat):
-        if pat[pt] == pat[pp]:
-            pt += 1
-            pp += 1
-            skip[pt] = pp
-        elif pp == 0:
-            pt += 1
-            skip[pt] = 0  # pp
+    while index != len(pat):
+        if pat[index] == pat[cnt]:
+            index += 1
+            cnt += 1
+            skip[index] = cnt
+        elif cnt == 0:
+            index += 1
+            skip[index] = 0  # cnt
         else:
-            pp = skip[pp]
+            cnt = skip[cnt]
 
     # ----- 내가 수정한 코드 -----
-    # while pt != len(pat):
-    #     if pat[pt] == pat[pp]:
-    #         pp += 1
-    #         pt += 1
-    #         skip[pt] = pp
+    # while index != len(pat):
+    #     if pat[index] == pat[cnt]:
+    #         cnt += 1
+    #         index += 1
+    #         skip[index] = cnt
     #     else:
-    #         if pp == 0:
-    #             pt += 1
-    #             skip[pt] = 0  # = pp
+    #         if cnt == 0:
+    #             index += 1
+    #             skip[index] = 0  # = cnt
     #         else:
-    #             pp = skip[pp]
+    #             cnt = skip[cnt]
 
-    pt = pp = 0
+    index = cnt = 0
 
-    while pt != len(txt) and pp != len(pat):
-        if txt[pt] == pat[pp]:
-            pt += 1
-            pp += 1
-        elif pp == 0:
-            pt += 1
-        else:
-            pp = skip[pp]
+    # ----- 내가 수정한 코드 -----
+    dist = 0
+    flag = -1
 
-    return pt - pp if pp == len(pat) else -1
+    while True:
+        index = 0
+        if (index + dist) + len(pat) > len(txt):
+            break
+
+        while txt[index + dist] == pat[cnt]:
+            cnt += 1
+            index += 1
+
+            if cnt == len(pat):
+                print(f'{dist + 1}번째에서 찾기 완료!')
+                flag += 1
+                break
+
+        dist = dist + (cnt - skip[cnt])
+        cnt = 0
+    return flag
+
+    # ----- 교재에 있는 코드 -----
+    # while index != len(txt) and cnt != len(pat):
+    #     if txt[index] == pat[cnt]:
+    #         index += 1
+    #         cnt += 1
+    #     elif cnt == 0:
+    #         index += 1
+    #     else:
+    #         cnt = skip[cnt]
+    #
+    # return index - cnt if cnt == len(pat) else -1
 
 
 if __name__ == '__main__':
@@ -49,13 +72,13 @@ if __name__ == '__main__':
     # s1 = input('텍스트 입력: ')
     # s2 = input('패턴 입력: ')
 
-    s1 = "abaabacabaaccabacabacabaacabacababcababcaac";
-    # s2 = "ABACABAAC";
-    s2='ababcababc'
+    s1 = 'ABAABACABAACCABACABACABAACABACABAAC'
+    s2 = "ABACABAAC"
 
     idx = kmp_match(s1, s2)
 
     if idx == -1:
         print('텍스트 안에 패턴이 존재하지 않습니다.')
     else:
-        print(f'{(idx + 1)}번째 문자가 일치합니다.')
+        # print(f'({idx + 1})번째 문자가 일치합니다.')
+        print(f'총({idx + 1})번의 문자열이 일치합니다.')
