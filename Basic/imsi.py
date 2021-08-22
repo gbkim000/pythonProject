94ac8e3ff34a21e46e3c8d12acf96a4d35485159
gbkim000 <gbkim1924@gmail.com>
2021-08-22 23:16:58 +0900

    8월 21일 오후 KMP 알고리즘 수정

---
+++ Basic/imsi.py
@@ -0,0 +1,11 @@
+class Person():
+    def __init__(Person, name, age, job):
+        Person.name = name
+        Person.age = age
+        Person.job = job
+
+
+Steve = Person('Steve', '22', 'programmer')
+print('이름:', Steve.name)
+print('나이:', Steve.age)
+print('직업:', Steve.job)
