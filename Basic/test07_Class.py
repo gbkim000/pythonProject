0d2c75c935e4a5efada7bd3d8412cc224cc3575e
gbkim000 <gbkim1924@gmail.com>
2022-08-05 12:52:08 +0900

    no message

--- Basic/test07_Class.py
+++ Basic/test07_Class.py
@@ -13,18 +13,15 @@ class Car:
 
     def __iter__(self):
         return iter(self.id)  # iter 함수로 변수 id의 iterator 객체를 반환
-    #####  ###
-
 
 def main():
     cc = Car('12가3456')
-    cc()  # __call__ 메서드 호출
+    cc()            # __call__ 메서드 호출
     print(len(cc))  # __len__ 메서드 호출
     print(str(cc))  # __str__ 메서드 호출
-    print(cc)  # __str__ 메서드 호출  / print(str(cc))와 동일
+    print(cc)       # __str__ 메서드 호출  / print(str(cc))와 동일
 
     for i in cc:  # Car 객체가 iterable 객체이므로 가능.(특수 메서드 __iter__(self))
         print(i, end=' ')
-
 
 main()
